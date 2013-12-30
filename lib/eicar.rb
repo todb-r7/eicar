3f5ee46d1f20c992b658507e13c08438e9bdc618
Tod Beardsley <todb@metasploit.com>
2013-12-30 11:09:30 -0600

    require 'eicar' now does something useful

--- lib/eicar.rb
+++ lib/eicar.rb
@@ -2,12 +2,27 @@ require 'eicar/version'
 
 module EICAR
 
-  def version
+  def self.version
     EICAR::VERSION
   end
 
-  def test_string
+  class Error < StandardError
+  end
 
+  class EICARReadError < Error
+  end
+
+  def self.test_string
+    lib_path = File.expand_path(File.dirname(__FILE__))
+    bin_path = File.expand_path(File.join(lib_path, "..", "bin"))
+    com_file = File.join(bin_path, "eicar.com")
+    begin
+    data = File.open(com_file, "rb") {|f| f.read f.stat.size}
+    rescue SystemCallError
+      raise EICAR::EICARReadError
+    end
   end
 
 end
+
+EICAR.test_string
