4de304fe913fa98a6470bef90b80f491ad52f54c
Tod Beardsley <todb@metasploit.com>
2013-12-31 17:03:39 -0600

    Refactor file path, add create method

--- lib/eicar.rb
+++ lib/eicar.rb
@@ -2,6 +2,9 @@ require 'eicar/version'
 
 module EICAR
 
+  # Antivirus should /not/ pick this up, ever.
+  ROT13_DOWNCASE_EICAR = "k5b!c%@nc[4cmk54(c^)7pp)7}$rvpne-fgnaqneq-nagvivehf-grfg-svyr!$u+u*"
+
   def self.version
     EICAR::VERSION
   end
@@ -12,12 +15,15 @@ module EICAR
   class EICARReadError < Error
   end
 
-  def self.test_string
+  def self.test_file_path
     lib_path = File.expand_path(File.dirname(__FILE__))
     bin_path = File.expand_path(File.join(lib_path, "..", "bin"))
-    com_file = File.join(bin_path, "eicar.com")
+    File.join(bin_path, "eicar.com")
+  end
+
+  def self.test_string
     begin
-    data = File.open(com_file, "rb") {|f| f.read f.stat.size}
+    data = File.open(self.test_file_path, "rb") {|f| f.read f.stat.size}
     rescue SystemCallError
       raise EICAR::EICARReadError
     end
@@ -30,6 +36,15 @@ module EICAR
       return true
     end
     return false
+  end
+
+  def self.rot13(str)
+    str.tr "A-Za-z", "N-ZA-Mn-za-m"
+  end
+
+  def self.create
+    data = self.rot13(ROT13_DOWNCASE_EICAR).upcase
+    File.open(self.test_file_path, "wb") {|f| f.write data}
   end
 
 end
