952b06f0d3e793b2b4f382003d7f7b4a1657df73
Tod Beardsley <todb@metasploit.com>
2013-12-31 17:10:18 -0600

    Allow for arbitrary writes.

--- lib/eicar.rb
+++ lib/eicar.rb
@@ -15,6 +15,9 @@ module EICAR
   class EICARReadError < Error
   end
 
+  class EICARWriteError < Error
+  end
+
   def self.test_file_path
     lib_path = File.expand_path(File.dirname(__FILE__))
     bin_path = File.expand_path(File.join(lib_path, "..", "bin"))
@@ -42,9 +45,22 @@ module EICAR
     str.tr "A-Za-z", "N-ZA-Mn-za-m"
   end
 
-  def self.create
+  # Returns a TrueClass if the file was created and is readable in the
+  # given path (by default, the expected path for the EICAR test file).
+  #
+  # For system-wide gem installs, this will usually need
+  # to be run as root, or else you'll raise.
+  #
+  # TODO: yardify this
+  def self.create(path=nil)
     data = self.rot13(ROT13_DOWNCASE_EICAR).upcase
-    File.open(self.test_file_path, "wb") {|f| f.write data}
+    begin
+      path ||= self.test_file_path
+      File.open(path, "wb") {|f| f.write data}
+    rescue SystemCallError
+      raise EICAR::EICARWriteError
+    end
+    File.readable? self.test_file_path
   end
 
 end
